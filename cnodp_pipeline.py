f4c99603198ebc22a1c6e6674f6afd46a6bbade2
Anono5044 <160368700+Anono5044@users.noreply.github.com>
2024-03-17 20:26:28 +0000

    Update cnodp_pipeline.py

--- cnodp_pipeline.py
+++ cnodp_pipeline.py
@@ -270,7 +270,7 @@ class Cnod:
 
                 projections.extend(customer_projection.to_dict(orient="records"))
                 loop_ctrler = loop_ctrler+1
-                if loop_ctrler > 230:
+                if loop_ctrler > 300:
                     break
 
             return pd.DataFrame(projections)
