a11b7314330526b8ee757b8bf2b56aed99a80a12
Anono5044 <160368700+Anono5044@users.noreply.github.com>
2024-03-18 13:30:32 +0000

    Update cnodpv8_0.py

--- cnodpv8_0.py
+++ cnodpv8_0.py
@@ -276,7 +276,8 @@ elif menu_option == "Benefits":
         #st.write(f"RMSE: {rmse:.2f}")
 
         # Predict next orders iteratively
-        projection_data = cnod.sim_predict_next_orders(test_data.copy(deep=True), model, 'Avg', 0, limit_number_cust) 
+        #projection_data = cnod.sim_predict_next_orders(test_data.copy(deep=True), model, 'Avg', 0, limit_number_cust) 
+        projection_data = cnod.sim_predict_next_orders(test_data[test_data.AccountNo.isin(baseline.AccountNo.unique()], model, 'Avg', 0, limit_number_cust)
         
         return raw, baseline, projection_data 
 
